0b29e2438b9823c2bb7cbb29a508ceb08b395d7f
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-04 17:12:58 -0500

    add `onClick`s to update scores

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   ***********************************************************/
   const points = {
     "touchdown" : 7,
-    "field_goal" : 3,
+    "fieldGoal" : 3,
   };
 
   /***********************************************************
@@ -71,12 +71,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeScore ((score) => (score + points.touchdown))}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore ((score) => (score + points.fieldGoal))}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setAwayScore ((score) => (score + points.touchdown))}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore ((score) => (score + points.fieldGoal))}>Away Field Goal</button>
         </div>
       </section>
     </div>
